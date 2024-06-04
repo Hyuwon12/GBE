0860966f13400c5ac83eae31c012f7fc99b3518d
Hyuwon12 <hyuwon12@gmail.com>
2024-06-04 10:20:37 +0900

    asd

--- ProjectGBE/src/main/java/com/kh/marathon/controller/ParticipateController.java
+++ ProjectGBE/src/main/java/com/kh/marathon/controller/ParticipateController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +14,7 @@ import com.kh.marathon.model.service.ParticipateService;
 import com.kh.marathon.model.service.RegionService;
 import com.kh.marathon.model.vo.Marathon;
 import com.kh.marathon.model.vo.Participate;
-
+@Controller
 public class ParticipateController {
 	@Autowired
 	private ParticipateService participateService;
